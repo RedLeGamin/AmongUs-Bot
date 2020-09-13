7c574f49b21d2f103463a076c6d0cfa2007f5c88
Glitch (hello-express) <none>
2020-09-13 20:20:17 +0000

    🐥😀 Checkpoint ./server.js:850652/10835 ./data/questions.json:850652/291

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ client.on("ready", async () => {
       return;
     }
     await channel.messages.fetch({ limit: 10 }).then(channel2 => {
-      console.log(channel2)
       console.log(channel.name + " fetched !");
     });
   }
@@ -57,16 +56,12 @@ client.on("message", message => {
   
   if (message.author.bot) return;
   if (message.channel.name.includes("ticket-") && command !== "close") {
-    console.log("tez");  
     const stepsdb = new FileSync("./data/steps.json");
     const db = low(stepsdb);
     var data = db.find({ channel: message.channel.id }).value();
     if (!db) return;
     if(data.state !== 0) return;
-    var machintruc = [
-      {"name": "red trop bg ?", "emoji": "💯"},
-      {"name": "autre truc", "emoji" : "😎"}
-    ]
+    var machintruc = require('./data/questions')
     var output = ""; 
     db.find({ channel: message.channel.id }).assign({state: 1}).write();
     machintruc.forEach((element => output += element.emoji + " " + element.name + "\n"))
@@ -99,6 +94,9 @@ client.on("message", message => {
   } 
 }); 
 
+// DETECTEUR DE PERSONNES QUI ARRÊTENT DE BOOST LE SERVEUR
+
+/*
 function log(eventName, options = {}) {
   message.channel.send();
 }
@@ -109,7 +107,7 @@ client.on("guildMemberUpdate", (oldMember, newMember) => {
   else if (oldMember.premiumSince == null && newMember.premiumSince != null)
     log("boost-start", { user: newMember.user });
 });
-
+*/
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
@@ -125,11 +123,10 @@ client.on("messageReactionAdd", async (reaction, user) => {
   let ticketid = await settings.get(`${reaction.message.guild.id}-ticket`);
 
   if (!ticketid) return;
-  console.log("penis")
   if (reaction.message.id == ticketid && reaction.emoji.name == "📩") {
     const stepsdb = new FileSync("./data/steps.json");
     const db = low(stepsdb);
-    console.log(db.find({ id: user.id }).value());
+    
 
     if (db.find({ id: user.id }).value()) return;
     reaction.users.remove(user);
