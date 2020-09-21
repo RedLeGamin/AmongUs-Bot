2315830d599a4d46e575a7c4025f9ba469b9a2b4
Glitch (hello-express) <none>
2020-09-21 20:38:57 +0000

    👘🌝 Checkpoint ./commands/test.js:850652/903

--- commands/test.js
+++ commands/test.js
@@ -3,19 +3,25 @@ exports.run = async (client, message, args, tools) => {
   if (!args[1]) return message.reply(".rename @someone nickaname");
   var member = args[0];
   
-  member = message.mentions.users.first();
   let nickname = args.slice(1).join(" ");
   
   var members = message.guild.members.array();
   if (member == "everyone") {
     for (var i = 0; i < members.length; i++) {
-      console.log(members[i]);
-      if (members[i].bot == true) continue;
-      await members[i].setNickname(nickname);
+      var tempMember = message.guild.members.get(members[i].id)
+      if (tempMember.bot == true) continue;
+      await tempMember.setNickname(nickname);
+      
     }
+    message.reply("Membres renommés en " + nickname)
     return;
   }
-  if(!member) member = message.mentions.users.first();
-  if(!member) member = message.mentions.users.first();
-};
+  member = message.mentions.users.first();
+  if(member) member = message.guild.members.get(member.id)
+  if(!member) member = message.guild.members.get(args[0])
+  if(!member) return message.reply("Membre introuvable")
+  console.log(member)
+  await member.setNickname(nickname)
+  message.reply("Membres renommés en " + nickname)
+
 };
