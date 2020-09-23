f3966ec19bffdeeedee440100e89a6cdafbdbf31
Glitch (childish-handy-island) <none>
2020-09-23 19:42:46 +0000

    🌆🍳 Checkpoint ./events/amongus.js:850652/342

--- events/amongus.js
+++ events/amongus.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args, tools) => {
   console.log(message.content.length)
-  if(message.length != 6) return;
+  console.log(message.content.slice(-1))
+  console.log(["Q","N"].includes(message.content.slice(-1)))
+  if(message.content.length != 6 && ["Q","N"].includes(message.content.slice(-1)) == false) return;
   else message.reply("Among us !")
 }
