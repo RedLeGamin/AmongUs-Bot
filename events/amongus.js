a7d6fa83b45da1705a5575d96d297160fbc205dc
Glitch (childish-handy-island) <none>
2020-09-23 20:30:46 +0000

    🏏🏙 Checkpoint ./events/amongus.js:850652/1728 ./server.js:850652/96

--- events/amongus.js
+++ events/amongus.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const linkCrack = require("./config.json").crack;
+const linkCrack = require("../config.json").linkCrack;
 exports.run = async (client, message, args, tools) => {
   if (
     message.content.length != 6 ||
@@ -10,11 +10,19 @@ exports.run = async (client, message, args, tools) => {
     " ",
     new Discord.MessageEmbed()
       .setAuthor(
-        "Partie Among Us",
+        "Partie Among Us - Invite de " + message.author.tag,
         "https://media.discordapp.net/attachments/405780210265620480/758414219732451328/among-us-icon.png"
       )
-      .addField("Code de la partie <:Liste:410856444813115393>", "`" + message.content.toLowerCase() + "`", true)
-      .addField("Dernier Crack du jeu", `[Lien médiafire](${linkCrack})`, true)
+      .addField(
+        "Code de la partie <:Liste:410856444813115393>",
+        "`" + message.content.toUpperCase() + "`",
+        true
+      )
+      .addField(
+        "Dernier Crack du jeu",
+        "[Lien médiafire](" + linkCrack + ")",
+        true
+      )
       .setColor("fcda42")
   );
 };
