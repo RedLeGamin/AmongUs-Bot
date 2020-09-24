6a2f85a3cb693096df93c45229c58f389d9e0758
Glitch (childish-handy-island) <none>
2020-09-24 17:29:51 +0000

    🐋🍇 Checkpoint ./commands/code.js:850652/11645

--- commands/code.js
+++ commands/code.js
@@ -1,30 +1,31 @@
 const Discord = require("discord.js");
+const linkCrack = require("../config.json").linkCrack;
+
 exports.run = (client, message, args, tools) => {
-  if (!args[0]) return message.reply(".code @someone");
   var member = args[0];
+  if (!member) member = message.author;
+  else {
+    member = message.mentions.users.first();
 
-  member = message.mentions.users.first();
+    if (member) member = message.guild.members.get(member.id);
+    if (!member) member = message.guild.members.get(args[0]);
+    if (!member) return message.reply("Membre introuvable");
+  }
+  var game = member.presence.game;
+  if (!game || game.name != "Among Us")
+    return message.reply(member + " ne joue pas à Among Us");
+  var party = game.party;
 
-  if (member) member = message.guild.members.get(member.id);
-  if (!member) member = message.guild.members.get(args[0]);
-  if (!member) return message.reply("Membre introuvable");
-
-  
-  var game = message.author.presence.game;
-  if (!game || game.name !="Among Us") return 
-    var party = game.party;
-    embed.addField("Place", `${party.size[0]}/${party.size[1]}`);
-  
-  
-    if (!party || !party.size || !party.size[1]) return;
+  if (!party || !party.size || !party.size[1])
+    return message.reply(member + " n'est pas en partie");
   const embed = new Discord.RichEmbed()
     .setAuthor(
-      "Partie Among Us - Invite de " + message.author.tag,
+      "Partie Among Us - Partie de " + member.tag,
       "https://media.discordapp.net/attachments/405780210265620480/758414219732451328/among-us-icon.png"
     )
     .addField(
       "Code de la partie <:Liste:410856444813115393>",
-      "`" + message.content.toUpperCase() + "`",
+      "`" + game.party.id + "`",
       true
     )
     .addField(
@@ -32,7 +33,16 @@ exports.run = (client, message, args, tools) => {
       "[Lien médiafire](" + linkCrack + ")",
       true
     )
-    .setColor("fcda42");
-  
+    .setColor("fcda42")
+    .setImage(
+      "https://cdn.discordapp.com/attachments/429158600720515077/758741106258608158/unknown.png"
+    );
+
+  embed.addField(
+    "Place",
+    `${party.size[0]}/${party.size[1]} <:Liste:410856444813115393>`,
+    true
+  );
+  embed.addField("Status", game.details, true);
   message.channel.send(embed);
 };
