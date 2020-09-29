d5f4bbb1f824bc83437030fcbff2e05d71baeff3
Glitch (childish-handy-island) <none>
2020-09-29 13:29:08 +0000

    😬🐁 Checkpoint ./ressources/getGame.js:850652/50

--- ressources/getGame.js
+++ ressources/getGame.js
@@ -1,9 +1,9 @@
 
 module.exports = class game {
   constructor(user) {
-    this.state = 0;
-    console.log(presence)
-    if(!presence) return undefined;
+    //this.state = 0;
+    //console.log(presence)
+    if(!user.ezffez) return null;
     this.state = 1;
   }
 };
